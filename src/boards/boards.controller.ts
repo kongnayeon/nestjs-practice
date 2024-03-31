5a015d288c40ab527e1799be574d59a92a7fb08e
kangnayeon <seoultownroad@naver.com>
2024-03-31 22:48:41 +0900

    feat: add getAllBoard service

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -4,4 +4,9 @@ import { BoardsService } from 'src/boards/boards.service';
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard() {
+    return this.boardService.getAllBoards();
+  }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards = [];
+  // private을 사용하지 않으면 다른 컴포넌트에서 boards값을 수정할 수 있음
+
+  getAllBoards() {
+    return this.boards;
+  }
+}
